09ecaa80a9d02da6f11d741475391434c7cd6c0d
Rafael Beserra <rafaelufrn@gmail.com>
2020-02-02 10:41:30 -0300

    Add files via upload

---
+++ proximoNumeroReal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+	int xint;
+	float x = 100000000;
+
+	memcpy(&xint, &x, sizeof(float));
+	xint += 1;
+	memcpy(&x, &xint, sizeof(float));
+
+	printf("%.012f %.012f\n", 0.2, x);
+
+	return 0;
+
+}
